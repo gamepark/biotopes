1ccf3e1a83c21db712872934b6640c9e1a69927b
Isilud <jolan.houot@gmail.com>
2025-03-17 16:01:20 +0100

    [TG-2 #ready-for-test] Fix: template renaming

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,7 +1,7 @@
 /** @jsxImportSource @emotion/react */
-import { GameTemplateOptionsSpec } from '@gamepark/game-template/GameTemplateOptions'
-import { GameTemplateRules } from '@gamepark/game-template/GameTemplateRules'
-import { GameTemplateSetup } from '@gamepark/game-template/GameTemplateSetup'
+import { BiotopesOptionsSpec } from '@gamepark/biotopes/BiotopesOptions'
+import { BiotopesRules } from '@gamepark/biotopes/BiotopesRules'
+import { BiotopesSetup } from '@gamepark/biotopes/BiotopesSetup'
 import { GameProvider, setupTranslation } from '@gamepark/react-game'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
@@ -16,10 +16,10 @@ setupTranslation(translations, { debug: false })
 ReactDOM.render(
   <StrictMode>
     <GameProvider
-      game="game-template"
-      Rules={GameTemplateRules}
-      optionsSpec={GameTemplateOptionsSpec}
-      GameSetup={GameTemplateSetup}
+      game="biotopes"
+      Rules={BiotopesRules}
+      optionsSpec={BiotopesOptionsSpec}
+      GameSetup={BiotopesSetup}
       material={Material}
       locators={Locators}
       animations={gameAnimations}
--- app/src/locators/Locators.ts
+++ app/src/locators/Locators.ts
@@ -1,6 +1,6 @@
-import { LocationType } from '@gamepark/game-template/material/LocationType'
-import { MaterialType } from '@gamepark/game-template/material/MaterialType'
-import { PlayerColor } from '@gamepark/game-template/PlayerColor'
+import { LocationType } from '@gamepark/biotopes/material/LocationType'
+import { MaterialType } from '@gamepark/biotopes/material/MaterialType'
+import { PlayerColor } from '@gamepark/biotopes/PlayerColor'
 import { Locator } from '@gamepark/react-game'
 
 export const Locators: Partial<Record<LocationType, Locator<PlayerColor, MaterialType, LocationType>>> = {}
