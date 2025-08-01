89b1685d0a2c1a785a9405ff4824417b2842cf02
Thibault HUTTIN-PASSERON <87776406+thuttinpasseron@users.noreply.github.com>
2025-08-01 23:47:03 +0200

    Feat: updated framework version, added polyhex coordinates for setup

--- rules/src/BiotopesSetup.ts
+++ rules/src/BiotopesSetup.ts
@@ -1,10 +1,17 @@
-import { MaterialGameSetup } from '@gamepark/rules-api'
+import { MaterialGameSetup, XYCoordinates } from '@gamepark/rules-api'
 import { BiotopesOptions } from './BiotopesOptions'
 import { BiotopesRules } from './BiotopesRules'
 import { LocationType } from './material/LocationType'
 import { MaterialType } from './material/MaterialType'
 import { PlayerColor } from './PlayerColor'
 import { RuleId } from './rules/RuleId'
+
+const polyHexCoordinates: Record<number, XYCoordinates[]> = {
+  2: [{ x: -2, y: -1 }, { x: 1, y: -2 }, { x: -1, y: 1 }, { x: 0, y: 2 }],
+  3: [{ x: -3, y: -1 }, { x: 0, y: -2 }, { x: 3, y: -3 }, { x: -2, y: 1}, { x: 1, y: 0 }, { x: -1, y: 3 }],
+  4: [{ x: 0, y: -3 }, { x: -2, y: 0 }, { x: 1, y: -1 }, { x: 4, y: -2 }, { x: -4, y: 3 }, { x: -1, y: 2 }, { x: 2, y: 1 }, { x: 0, y: 4 }],
+  5: [{ x: 0, y: -3 }, { x: 3, y: -4 }, { x: -2, y: 0 }, { x: 1, y: -1 }, { x: 4, y: -2 }, { x: -4, y: 3 }, { x: -1, y: 2}, { x: 2, y: 1 }, { x: -3, y: 5 }, { x: 0, y: 4 }],
+}
 
 /**
  * This class creates a new Game based on the game options
