896925b8c9013d46147edaf05f00355cd264f4f4
Isilud <jolan.houot@gmail.com>
2025-08-19 20:21:50 +0200

    feat : add environmental token description

--- app/src/material/Material.ts
+++ app/src/material/Material.ts
@@ -5,11 +5,13 @@ import { ecosystemBoardDescription } from './EcosystemBoardDescription'
 import { environmentalConditionsBoardDescription } from './EnvironmentalConditionsBoardDescription'
 import { initiativeTokenDescription } from './InitiativeTokenDescription'
 import { landscapeTileDescription } from './LandscapeTileDescription'
+import { environmentalConditionTokenDescription } from './EnvironmentalConditionTokenDescription'
 
 export const Material: Partial<Record<MaterialType, MaterialDescription>> = {
   [MaterialType.CycleToken]: cycleTokenDescription,
   [MaterialType.EcosystemBoard]: ecosystemBoardDescription,
   [MaterialType.EnvironmentalConditionsBoard]: environmentalConditionsBoardDescription,
+  [MaterialType.EnvironmentalConditionToken]: environmentalConditionTokenDescription,
   [MaterialType.InitiativeToken]: initiativeTokenDescription,
   [MaterialType.LandscapeTile]: landscapeTileDescription
 }
