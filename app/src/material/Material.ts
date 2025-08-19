c064cd7c06fadcd6fc6062cc7315bfdf7973b69a
Isilud <jolan.houot@gmail.com>
2025-08-19 19:28:26 +0200

    feat : add cycle token description

--- app/src/material/Material.ts
+++ app/src/material/Material.ts
@@ -1,11 +1,13 @@
 import { MaterialType } from '@gamepark/biotopes/material/MaterialType'
 import { MaterialDescription } from '@gamepark/react-game'
+import { cycleTokenDescription } from './CycleTokenDescription'
 import { ecosystemBoardDescription } from './EcosystemBoardDescription'
 import { environmentalConditionsBoardDescription } from './EnvironmentalConditionsBoardDescription'
 import { initiativeTokenDescription } from './InitiativeTokenDescription'
 import { landscapeTileDescription } from './LandscapeTileDescription'
 
 export const Material: Partial<Record<MaterialType, MaterialDescription>> = {
+  [MaterialType.CycleToken]: cycleTokenDescription,
   [MaterialType.EcosystemBoard]: ecosystemBoardDescription,
   [MaterialType.EnvironmentalConditionsBoard]: environmentalConditionsBoardDescription,
   [MaterialType.InitiativeToken]: initiativeTokenDescription,
