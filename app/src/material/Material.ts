f1f3840702a9e6ed557c809fb3ee7fe562225792
Isilud <jolan.houot@gmail.com>
2025-08-20 16:34:31 +0200

    [TG-5 #in-progress] feat : biotope card images and description

--- app/src/material/Material.ts
+++ app/src/material/Material.ts
@@ -1,13 +1,15 @@
 import { MaterialType } from '@gamepark/biotopes/material/MaterialType'
 import { MaterialDescription } from '@gamepark/react-game'
 import { cycleTokenDescription } from './CycleTokenDescription'
+import { biotopeCardDescription } from './BiotopeCardDescription'
 import { ecosystemBoardDescription } from './EcosystemBoardDescription'
 import { environmentalConditionsBoardDescription } from './EnvironmentalConditionsBoardDescription'
+import { environmentalConditionTokenDescription } from './EnvironmentalConditionTokenDescription'
 import { initiativeTokenDescription } from './InitiativeTokenDescription'
 import { landscapeTileDescription } from './LandscapeTileDescription'
-import { environmentalConditionTokenDescription } from './EnvironmentalConditionTokenDescription'
 
 export const Material: Partial<Record<MaterialType, MaterialDescription>> = {
+  [MaterialType.BiotopesCard]: biotopeCardDescription,
   [MaterialType.CycleToken]: cycleTokenDescription,
   [MaterialType.EcosystemBoard]: ecosystemBoardDescription,
   [MaterialType.EnvironmentalConditionsBoard]: environmentalConditionsBoardDescription,
