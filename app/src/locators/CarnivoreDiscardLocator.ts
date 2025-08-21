30d45b5e7b6a9f2d50456db1786b6a67aa523f07
Isilud <jolan.houot@gmail.com>
2025-08-21 21:57:13 +0200

    feat : add discard

---
+++ app/src/locators/CarnivoreDiscardLocator.ts
@@ -0,0 +1,16 @@
+import { DeckLocator, MaterialContext } from '@gamepark/react-game'
+import { Coordinates, Location } from '@gamepark/rules-api'
+import { LocationType } from '@gamepark/biotopes/material/LocationType'
+import { MaterialType } from '@gamepark/biotopes/material/MaterialType'
+import { PlayerColor } from '@gamepark/biotopes/PlayerColor'
+import { centralLandscapeLocator } from './CentralLandscapeLocator'
+
+class CarnivoreDiscardLocator extends DeckLocator<PlayerColor, MaterialType, LocationType> {
+  maxCount = 10
+  getCoordinates(_location: Location<PlayerColor, LocationType>, context: MaterialContext<PlayerColor, MaterialType, LocationType>): Partial<Coordinates> {
+    const landscapeSize = centralLandscapeLocator.getLandscapeSize(context.rules.game)
+    return { x: landscapeSize.width / 2 + 24, y: -12 }
+  }
+}
+
+export const carnivoreDiscardLocator = new CarnivoreDiscardLocator()
