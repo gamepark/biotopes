18b78c187e7ea555a83ea0f36df92b208d756bba
Isilud <jolan.houot@gmail.com>
2025-08-21 21:57:13 +0200

    feat : add discard

--- app/src/locators/Locators.ts
+++ app/src/locators/Locators.ts
@@ -13,17 +13,23 @@ import { advancedBiotopesSelectionLocator } from './AdvancedBiotopesSelectionLoc
 import { herbivoreDeckLocator } from './HerbivoreDeckLocator'
 import { carnivoreDeckLocator } from './CarnivoreDeckLocator'
 import { insectivoreDeckLocator } from './InsectivoreDeckLocator'
+import { carnivoreDiscardLocator } from './CarnivoreDiscardLocator'
+import { herbivoreDiscardLocator } from './HerbivoreDiscardLocator'
+import { insectivoreDiscardLocator } from './InsectivoreDiscardLocator'
 
 export const Locators: Partial<Record<LocationType, Locator<PlayerColor, MaterialType, LocationType>>> = {
   [LocationType.AdvancedBiotopesSelectionSpot]: advancedBiotopesSelectionLocator,
   [LocationType.CarnivoreDeckSpot]: carnivoreDeckLocator,
+  [LocationType.CarnivoreDiscardSpot]: carnivoreDiscardLocator,
   [LocationType.CentralLandscapeSpot]: centralLandscapeLocator,
   [LocationType.CycleTokenSpotOnEnviromnentalConditionsBoard]: cycleTokenLocator,
   [LocationType.EcosystemBoardSpot]: ecosystemBoardLocator,
   [LocationType.EnvironmentalConditionsBoardSpot]: environmentalConditionsBoardLocator,
   [LocationType.EnvironmentalConditionTokenSpotOnEnviromnentalConditionsBoard]: environmentalConditionsTokenLocator,
   [LocationType.HerbivoreDeckSpot]: herbivoreDeckLocator,
+  [LocationType.HerbivoreDiscardSpot]: herbivoreDiscardLocator,
   [LocationType.InsectivoreDeckSpot]: insectivoreDeckLocator,
+  [LocationType.InsectivoreDiscardSpot]: insectivoreDiscardLocator,
   [LocationType.PlayerBiotopesCardSpot]: playerBiotopeCardsLocator,
   [LocationType.PlayerInitiativeTokenSpot]: initiativeTokenLocator
 }
