d439a2d89dcc981c78bbf7130e68897d929d4451
Isilud <jolan.houot@gmail.com>
2025-08-21 22:55:03 +0200

    fix : changed to positionOnParent

--- app/src/locators/EnvironmentalConditionsTokenLocator.ts
+++ app/src/locators/EnvironmentalConditionsTokenLocator.ts
@@ -8,12 +8,10 @@ class EnvironmentalConditionsTokenLocator extends ListLocator {
   parentItemType = MaterialType.EnvironmentalConditionsBoard
   gap = { x: 2.73 }
 
+  positionOnParent = { x: 11.5, y: 25.5 }
+
   getParentItem(_location: Location, context: MaterialContext): MaterialItem<PlayerColor> | undefined {
     return environmentalConditionsBoardDescription.getStaticItems(context)[0]
-  }
-
-  getPositionOnParent(_location: Location, _context: MaterialContext) {
-    return { x: 11.5, y: 25.5 }
   }
 }
 
